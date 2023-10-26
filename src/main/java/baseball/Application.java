4748bbc3278ed9c164b2823d833bc7a2f95090a6
IToriginal <gitshineit@gmail.com>
2023-10-27 02:29:04 +0900

    :sparkles: [feature] 몇개의 숫자가 같은지 알 수 있는 기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,14 +1,18 @@
 package baseball;
 
-import baseball.domain.NumberGenerator;
-import java.util.List;
+import baseball.domain.Judgment;
+import java.util.Arrays;
 
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        NumberGenerator generator = new NumberGenerator();
-        List<Integer> numbers = generator.createRandomNumbers();
-        System.out.println(numbers);
+//        NumberGenerator generator = new NumberGenerator();
+//        List<Integer> numbers = generator.createRandomNumbers();
+//        System.out.println(numbers);
+
+        Judgment judgment = new Judgment();
+        int count = judgment.correctCount(Arrays.asList(2, 8, 3), Arrays.asList(1, 2, 3));
+        System.out.println(count);
     }
 }
